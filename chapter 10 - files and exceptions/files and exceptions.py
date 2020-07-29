b48f08599ec630e19ab6d8582a9b8eadf3f0172f
yoelgama <joeldson1997@gmail.com>
2020-07-29 13:45:49 -0300

    três métodos diferentes de se abrir e ler um arquivo

--- chapter 10 - files and exceptions/files and exceptions.py
+++ chapter 10 - files and exceptions/files and exceptions.py
@@ -1,4 +1,18 @@
 with open("files/pi_digits.txt") as file:
     content = file.read()
+    print(content.rstrip())
 
-print(content.rstrip())
+if file.closed:
+    print("Fechou!")
+
+for line in open("files/pi_digits.txt"):
+    print(line)
+
+file = open("files/pi_digits.txt")
+
+for line in file:
+    print(line.rstrip())
+
+file.close()
+if file.closed:
+    print("Fechou!")

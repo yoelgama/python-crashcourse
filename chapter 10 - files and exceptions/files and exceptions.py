815b88e7f105af8608f698c578b7f645f691fb6b
yoelgama <joeldson1997@gmail.com>
2020-07-29 17:46:20 -0300

    forma de se ler um arquivo está pronta

--- chapter 10 - files and exceptions/files and exceptions.py
+++ chapter 10 - files and exceptions/files and exceptions.py
@@ -1,18 +1,45 @@
-with open("files/pi_digits.txt") as file:
-    content = file.read()
-    print(content.rstrip())
+# with open("files/pi_digits.txt") as file:
+#     content = file.read()
+#     print(content.rstrip())
+#
+# if file.closed:
+#     print("Fechou!")
+#
+# for line in open("files/pi_digits.txt"):
+#     print(line)
+#
+# file = open("files/pi_digits.txt")
+#
+# for line in file:
+#     print(line.rstrip())
+#
+# file.close()
+# if file.closed:
+#     print("Fechou!")
+#
+# lines = open("files/pi_digits.txt").readlines()
+# print(lines)
+#
+# for line in lines:
+#     print(line.rstrip())
+#
+# birth = input("Seu aniversário aparece nos primeiros um milhão de digitos PI? Insira-o aqui: ")
+#
+# if birth in open("files/pi_million_digits.txt").read().rstrip():
+#     print("Yep, seu aniversário está em PI!")
+# else:
+#     print("No, sorry")
 
-if file.closed:
-    print("Fechou!")
-
-for line in open("files/pi_digits.txt"):
-    print(line)
-
-file = open("files/pi_digits.txt")
-
-for line in file:
-    print(line.rstrip())
-
-file.close()
-if file.closed:
-    print("Fechou!")
+try:
+    dividendo = int(input("dividendo: "))
+    print("/")
+    divisor = int(input("divisor: "))
+except ValueError:
+    print("Vai dividir letra, arrombado?")
+else:
+    try:
+        resposta = dividendo / divisor
+    except ZeroDivisionError:
+        print("You funking donkey")
+    else:
+        print(resposta)
